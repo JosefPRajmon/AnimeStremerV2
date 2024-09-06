8a87ada1d250b326f00b1cc531df4a8c4a8e51f4
Josef Rajmon <rajmon.josef1999@seznam.cz>
2024-09-06 17:37:17 +0100

    Add description for episode

--- Migrations/20240906163453_AddDescriptionForEpisode.Designer.cs
+++ Migrations/20240906163453_AddDescriptionForEpisode.Designer.cs
@@ -4,6 +4,7 @@ using AnimeStreamerV2.DbContextFile;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace test.Migrations
 {
     [DbContext(typeof(AnimeDbContext))]
-    partial class AnimeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240906163453_AddDescriptionForEpisode")]
+    partial class AddDescriptionForEpisode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -83,9 +86,6 @@ namespace test.Migrations
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("Rating")
-                        .HasColumnType("int");
 
                     b.HasKey("Id");
 
