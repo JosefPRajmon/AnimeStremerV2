cdf3c2c3854c3f3b6ab79cf8cd132e58b4378eae
Josef Rajmon <rajmon.josef1999@seznam.cz>
2024-09-07 15:35:56 +0100

    Redisign  mainpage add system by country add

---
+++ Migrations/20240907125254_AddCountry.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace test.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EpisodaIcon",
+                table: "Episodes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryOfOrigin",
+                table: "Animes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EpisodaIcon",
+                table: "Episodes");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CountryOfOrigin",
+                table: "Animes");
+        }
+    }
+}
